d54f62468b5368a09ac453befe31f558b6fb84ca
KohsukeIde <kohsukeide0726@gmail.com>
2023-10-02 02:05:20 +0900

    feat: clusterRoleBindings

--- main.go
+++ main.go
@@ -4,16 +4,21 @@ import (
 	"bufio"
 	"context"
 	"fmt"
-	
+	"log"
+	"sync"
+	"time"
+
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"sync"
 )
 
-func streamLogs(ctx context.Context,clientset *kubernetes.Clientset, namespace, podName string, wg *sync.WaitGroup) {
-	defer wg.Done()
+func streamLogs(ctx context.Context, clientset *kubernetes.Clientset, namespace, podName string) {
+	// Exclude the log-streamer pod itself to prevent a log loop
+	if podName == "log-streamer" {
+		return
+	}
 
 	podLogOpts := v1.PodLogOptions{
 		Follow: true,
@@ -21,7 +26,7 @@ func streamLogs(ctx context.Context,clientset *kubernetes.Clientset, namespace, 
 	req := clientset.CoreV1().Pods(namespace).GetLogs(podName, &podLogOpts)
 	podLogs, err := req.Stream(ctx)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
 	defer podLogs.Close()
@@ -34,47 +39,50 @@ func streamLogs(ctx context.Context,clientset *kubernetes.Clientset, namespace, 
 }
 
 func fetchPods(ctx context.Context, namespace string) ([]v1.Pod, *kubernetes.Clientset, error) {
-	var config *rest.Config
-	var err error
-
-	config, err = rest.InClusterConfig()
+	config, err := rest.InClusterConfig()
 	if err != nil {
-		fmt.Println("Error reading config:", err)
-		panic(err)
+		log.Println("Error reading config:", err)
+		return nil, nil, err
 	}
 
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		fmt.Println("Error creating clientset:", err)
-		panic(err)
+		log.Println("Error creating clientset:", err)
+		return nil, nil, err
 	}
 
 	podList, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
-		fmt.Println("Error fetching pods:", err)
-		panic(err)
+		log.Println("Error fetching pods:", err)
+		return nil, nil, err
 	}
-	pods := make([]v1.Pod, 0)
-	for _, pod := range podList.Items {
-		pods = append(pods, pod)
-	}
+	pods := append([]v1.Pod{}, podList.Items...)
 	return pods, clientset, err
 }
 
 func main() {
-	namespace := "ide-local"
+	namespace := "ide"
 	ctx := context.Background()
 
-	pods, clientset, err := fetchPods(ctx, namespace)
-	if err != nil {
-		panic(err)
-	}
+	for {
+		pods, clientset, err := fetchPods(ctx, namespace)
+		if err != nil {
+			log.Println("Error fetching pods:", err)
+			time.Sleep(10 * time.Second)
+			continue
+		}
 
-	var wg sync.WaitGroup
-	for _, pod := range pods {
-		wg.Add(1)
-		go streamLogs(ctx, clientset, namespace, pod.Name, &wg)
-	}
+		var wg sync.WaitGroup
+		for _, pod := range pods {
+			wg.Add(1)
+			go func(pod v1.Pod) {
+				defer wg.Done()
+				streamLogs(ctx, clientset, namespace, pod.Name)
+			}(pod)
+		}
 
-	wg.Wait()
+		wg.Wait()
+		log.Println("All log streaming goroutines completed, restarting in 10 seconds...")
+		time.Sleep(10 * time.Second)
+	}
 }
